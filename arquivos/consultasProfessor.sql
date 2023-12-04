aa8bdd83ab3dc5f18e7e0d5a3b265c2b866c616d
Miguel <140199292+MiguelCSLima@users.noreply.github.com>
2023-12-03 23:16:17 -0300

    Update consultasProfessor.sql
    Alteração nas consultas 3 e 5.

--- arquivos/consultasProfessor.sql
+++ arquivos/consultasProfessor.sql
@@ -14,12 +14,15 @@ GROUP BY F.ID
 HAVING COUNT(PF.ID) >= 2;
 
 -- 3. Retornar o nome do cliente, a quantidade e o valor total dos seus pedidos.
-SELECT C.nome AS Nome_Cliente, COUNT(NF.ID) AS Quantidade_Pedidos, ROUND(SUM(Pr.preco), 2) AS Valor_Total
+SELECT C.nome AS NomeCliente,
+P.ID AS ID_Pedido,
+PP.quantidade AS Quantidade,
+ROUND((PP.quantidade * PR.preco), 2) AS ValorTotal
 FROM Cliente C
 JOIN NotaFiscal NF ON C.ID = NF.ID_Cliente
 JOIN Pedido P ON NF.ID = P.ID_NotaFiscal
-JOIN Produto Pr ON P.ID_Produto = Pr.ID
-GROUP BY C.ID;
+JOIN PedidoProduto PP ON P.ID = PP.ID_Pedido
+JOIN Produto PR ON PP.ID_Produto = PR.ID;
 
 -- 4. Listar o id, a descrição e o nome dos produtos, além dos ingredientes usados para prepará-los.
 SELECT P.ID AS ID_Produto, P.descricao AS Descricao_Produto,
@@ -30,7 +33,7 @@ JOIN ProdutoIngrediente PI ON P.ID = PI.ID_Produto
 JOIN Ingrediente I ON PI.ID_Ingrediente = I.ID;
 
 -- 5. Listar a descrição e o preço e a descrição dos produtos que nunca foram vendidos.
-SELECT P.descricao AS Descricao_Produto, P.preco AS Preco_Produto
+SELECT P.descricao AS DescricaoProduto,
+P.preco AS Preco
 FROM Produto P
-LEFT JOIN Pedido PD ON P.ID = PD.ID_Produto
-WHERE PD.ID IS NULL;
+WHERE P.ID NOT IN (SELECT DISTINCT ID_Produto FROM PedidoProduto);
